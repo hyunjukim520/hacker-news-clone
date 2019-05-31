61c52ed7f7de6e6b6d44d51e1948e5e16297dfe8
hyunju@Julie-Kim <hyunju.kim520@gmail.com>
2019-05-31 13:20:22 +1000

    Updates

---
+++ precache-manifest.d836c9092f8907a3c7a71438940207ba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa6640c3bed3bb4da96b01963335d2c",
+    "url": "/hacker-news-clone/index.html"
+  },
+  {
+    "revision": "f1c1e8529b485ebc7118",
+    "url": "/hacker-news-clone/static/js/2.f41009fc.chunk.js"
+  },
+  {
+    "revision": "a78ad385607d53becaab",
+    "url": "/hacker-news-clone/static/js/main.69df8ba2.chunk.js"
+  },
+  {
+    "revision": "6a54c7f230b9f1d0e3d1",
+    "url": "/hacker-news-clone/static/js/runtime~main.cd7531b5.js"
+  }
+]);
